f2d4050c967fa2cca73c9e56b03280f1af4a30d3
Sebastian Alvarado <sebastian.alvarado@mail.utoronto.ca>
2016-12-02 14:29:26 -0500

    Description table of innings is working

--- assets/scripts/games.js
+++ assets/scripts/games.js
@@ -7,7 +7,7 @@ var gamesApp = {};
 var favTeam = "Blue Jays";
 //Data gathered from the API
 var data = {};
-
+var data_desc = {};
 var monToString = {"01":"January", "02":"February", "03": "March",
   "04":"April","05":"May","06":"June","07":"July", "08":"August","09":"September",
 "10":"October","11":"November","12":"December"};
@@ -21,7 +21,84 @@ function produceTitleDate(){
   console.log(result);
   $("#content").find("#headerDate").append(result);
 }
+/** Helper function that produces the `table` to be displayed and appends it to the DOM
+* @param headers innings is how many innings there are
+* @param home_innings Array having the score for that innings
+* @param away_innings ''
+* post-condition:
+*/
+function produceTable(innings,home_innings,away_innings,home,away){
+  var table = "<table class='table table-hover'>";
+  // Produce the headers
+  table += "<tr class='heading'>";
+  //Each header gets populated
+  // Add the pivot
+  table += "<th>City</th>";
+  for(i = 1; i < innings; i++){
+    table += "<th>" + i + "</th>"
+  }
+  //Closing header
+  table += "</tr>";
+// Loop invariant: every row will add a team's score
+for(i = 0; i < innings; i++ ){
+  if (i==0){
+    // Add the name
+    table += "<tr><td>" + home + "</td>";
+  }
+  else{
+    table += "<td>" + home_innings[i] + "</td>";
+  }
+}
+  table += "</tr>";
+for(i = 0; i < innings; i++){
+  if (i==0){
+    // Add the name
+    table += "<tr><td>" + away + "</td>";
+  }
+  else{
+    table += "<td>" + away_innings[i] + "</td>";
+  }
+}
+table += "</tr>";
+// Close the table tag
+table += "</table>"
+//Append it to the div
+$(".container").find("#content").find("#description").append(table);
+}
 
+/**
+ *
+ */
+gamesApp.populateDescription = function(){
+  // Get the array of linescore by innings
+  var linescore = data_desc.data.boxscore.linescore.inning_line_score;
+  var home_innings = [];
+  var away_innings = [];
+  // Loop through every inning
+  for(i = 0; i < linescore.length; i++){
+    home_innings.push(linescore[i].home);
+    away_innings.push(linescore[i].away);
+  }
+  // Get the team name
+  var home_team = data_desc.data.boxscore.home_sname;
+  var away_team = data_desc.data.boxscore.away_fname;
+  // Call the table to get populated
+  produceTable(linescore.length, home_innings, away_innings,home_team,away_team);
+}
+
+/**
+ */
+gamesApp.getDescResults = function(url){
+  $.get(url,function(response){
+    // Data successfully gathered
+    data_desc = response;
+    //Empty the previous list
+    $("#content").find(".list-group").empty();
+    $("#content").find("#headerDate").empty();
+    // Send it to populate Desc
+    gamesApp.populateDescription();
+  });
+}
 
 /** Helper function that renders one list entry in the list view
  *
@@ -54,6 +131,13 @@ gamesApp.renderListEntry = function(game,index){
   //Append it to the list
   var list = $(".container").find("#content").find(".list-group");
   list.append(entry);
+  // Add an onClick listener
+  list.find("#" + index).on('click', function(e){
+    // Get the URL that is in value of the attribute
+    var url = $(this).attr("value");
+    var total_url = "http://gd2.mlb.com/" + url;
+    gamesApp.getDescResults(total_url);
+  });
   // Highlight the winners name
   var winner = (score_home >= score_away? 'home':'away');
   list.find("#" + index).find("#" + winner).effect("highlight", {color:"#FB4D3D"},300000000000);
@@ -73,23 +157,44 @@ gamesApp.getResults = function(query){
   });
 }
 
+/** Helper function that lets us order the results by favourite team first
+ *
+ */
+gamesApp.orderedResults = function(results){
+  // Loop over the array
+  var favouriteGame;
+  for(i = 0; i < results.length;i++){
+      // When you find the preferred team delete it from array
+      // Get the names fo both teams
+      var team_1 = results[i].home_team_name;
+      var team_2 = results[i].away_team_name;
+      if(favTeam == team_1 || favTeam == team_2){
+        favouriteGame = results[i];
+        results.splice(i,1);
+      }
+  }
+  // Push it at the beginning
+  results.unshift(favouriteGame);
+  // Return ot
+  return results;
+}
 /** Helper function that populates the listview with the received data
  *
  */
 gamesApp.populateList = function(data){
   // Check if the data is an object or an array
   var gamesData = data.data.games;
-  console.log(data.data.games);
   if(typeof gamesData.game == 'undefined'){
     //Render one list element with no matches
     var entry = "<a href='#' class='list-group-item active'>";
-    entry += "<h4 class='list-group-item-heading'>No games in this day</h4></a>";
+    entry += "<h4 class='list-group-item-heading'>No games today</h4></a>";
     $(".container").find("#content").find(".list-group").append(entry);
     return;
   }
   // Check if game is an array or an object
   if(gamesData.game instanceof Array){
     // TODO: Order the array having the preferred team first
+    gamesData.game = gamesApp.orderedResults(gamesData.game);
     for(i = 0; i < gamesData.game.length; i++){
       // Produce one list entry per game
       gamesApp.renderListEntry(gamesData.game[i],i);
@@ -134,10 +239,8 @@ gamesApp.init = function(){
   // Produce the title view
   // Populate the listview with our default date
 }
+
 $(document).ready(function() {
   // Add on change or click listener to the date picker
   gamesApp.init();
-  // Populate the results
-  // Add onClickListener to every li
-
 });
